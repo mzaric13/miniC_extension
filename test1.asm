7cbee1ec1d8534fa58dbd7338dfbe8832af3a4e6
Matija Zaric <matijazaric9@gmail.com>
2022-06-10 14:40:12 +0200

    Add array declarations

---
+++ test1.asm
@@ -0,0 +1,22 @@
+
+func:
+		PUSH	%14
+		MOV 	%15,%14
+		SUBS	%15,$24,%15
+@func_body:
+		MOV 	$0,%13
+		JMP 	@func_exit
+@func_exit:
+		MOV 	%14,%15
+		POP 	%14
+		RET
+main:
+		PUSH	%14
+		MOV 	%15,%14
+@main_body:
+		MOV 	$0,%13
+		JMP 	@main_exit
+@main_exit:
+		MOV 	%14,%15
+		POP 	%14
+		RET
---
+++ test1.mc
@@ -0,0 +1,11 @@
+// RETURN 0
+
+int func() {
+	int a[3];
+	int b[] = {1, 2, 3};
+	return 0;
+}
+
+int main() {
+	return 0;
+}
